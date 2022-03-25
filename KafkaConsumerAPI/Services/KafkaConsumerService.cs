76ce5768ca19e95106d3f143876c67f7796deb78
eswaribala <parameswaribala@gmail.com>
2022-03-25 10:46:26 +0530

    updated

--- KafkaConsumerAPI/Services/KafkaConsumerService.cs
+++ KafkaConsumerAPI/Services/KafkaConsumerService.cs
@@ -1,10 +1,59 @@
-﻿namespace KafkaConsumerAPI.Services
+﻿using Confluent.Kafka;
+
+namespace KafkaConsumerAPI.Services
 {
     public class KafkaConsumerService : IHostedService
     {
+        private IConfiguration _config;
+        private ILogger<KafkaConsumerService> _logger;
+        public KafkaConsumerService(IConfiguration configuration, 
+            ILogger<KafkaConsumerService> logger)
+        {
+            this._config = configuration;
+            this._logger = logger;  
+        }
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var config = new ConsumerConfig
+            {
+                GroupId = "test_group",
+                BootstrapServers = _config["BootStrapServer"],
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            try
+            {
+                using (var consumerBuilder = new ConsumerBuilder
+                <Ignore, string>(config).Build())
+                {
+                    consumerBuilder.Subscribe(_config["TopicName"]);
+                    var cancelToken = new CancellationTokenSource();
+
+                    try
+                    {
+                        while (true)
+                        {
+                            var consumer = consumerBuilder.Consume
+                               (cancelToken.Token);
+                            var walletRequest = consumer.Message.Value;
+
+                            _logger.LogInformation($"Wallet Amount:{ walletRequest}");
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        consumerBuilder.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+
+
+            return Task.CompletedTask;
+
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
